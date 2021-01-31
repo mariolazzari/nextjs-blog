0d8a1ab3d2ca42409fd70becb0e2ea9f68c45719
Mario Lazzari <mario.lazzari@gmail.com>
2021-01-31 13:48:19 +0100

    Done css module

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,18 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/Layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First post</h1>
       <Link href="/">
         <a>Back to Home</a>
       </Link>
-    </>
+    </Layout>
   );
 }
